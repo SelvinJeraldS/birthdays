2d45f8a131c899e70bbfac58541f77c00036cceb
SelvinJeraldS <89121433+SelvinJeraldS@users.noreply.github.com>
2023-07-22 13:44:44 +0530

    added script

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+function revealMessage() {
+  // Get the slides container and the individual slides
+  const slidesContainer = document.querySelector('.slides');
+  const slide1 = document.getElementById('slide1');
+  const slide2 = document.getElementById('slide2');
+  const slide3=document.getElementById('slide3');
+
+  // Hide the first slide and show the second slide when the gift box is clicked
+  slide1.style.display = 'none';
+  slide2.style.display = 'block';
+  slide3.style.display='none';
+}
+
+function test(){
+  href="marque.html"
+}
